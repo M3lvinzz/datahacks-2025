344c9979317378a1ba70ede4f279a81f688bd14b
jon009@ucsd.edu <jon009@ucsd.edu>
2025-04-06 01:13:54 -0700

    updated game_reviews.py with joblib

--- game_reviews.py
+++ game_reviews.py
@@ -1,28 +1,35 @@
 import streamlit as st
 import pandas as pd
 import time
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import classification_report
 
-
+st.logo(image = 'racoon.jpg')
 st.markdown("<h1 style='text-align: center; color: black;'>Genre Prediction based on Reviews </h1>", unsafe_allow_html=True)
 st.write('Inspired by our love for video games, we decided to build a prediction model that can distinguish what type of genre a video game is based on the review input down below.')
 
 
-data = pd.read_csv('games.csv')
-
-st.logo(image = 'racoon.jpg')
+data = pd.read_csv('final_data.csv')
+st.write(data)
 
 
 input = st.text_input("Please enter a review and we'll predict the genre",)
 
-output = ''
+loaded_model = joblib.load('genre_classifier_model.joblib')
 
-accuracy_model = ""
+output = loaded_model.predict([input])
 
 if st.button('Run input'):
     with st.spinner("Wait for it...", show_time=True):
-        time.sleep(3)
-        st.success(input)
+        time.sleep(1)
+        st.success(output)
 
 st.write('The accuracy of our model is: ')
-
+#st.write(classification_report())
 

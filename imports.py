4a3a1e6b1b61abc2b0bbc1f8609ef502dcc949f5
M3lvinzz <melvinsasylum@gmail.com>
2025-04-06 13:24:39 -0700

    new model

--- imports.py
+++ imports.py
@@ -10,3 +10,13 @@ import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.io as pio
+import re
+import ast
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.metrics import classification_report
+from sklearn.pipeline import make_pipeline
